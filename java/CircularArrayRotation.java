79148725327a2bbdfaf2ff0fa7e5e1cd6fb25d31
Ilyess <rachedi.ilyess@gmail.com>
2024-03-21 21:05:50 +0100

    new

---
+++ java/CircularArrayRotation.java
@@ -0,0 +1,50 @@
+/*
+Problem : Circular Array Rotation
+----------------------------------------------------------------------------------------------------------
+John Watson knows of an operation called a right circular rotation on an array of integers.
+One rotation operation moves the last array element to the first position and shifts all remaining elements right one. 
+To test Sherlock's abilities, Watson provides Sherlock with an array of integers. 
+Sherlock is to perform the rotation operation a number of times then determine the value of the element at a given position.
+
+For each array, perform a number of right circular rotations and return the values of the elements at the given indices.
+See more : https://www.hackerrank.com/challenges/circular-array-rotation/problem
+*/
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CircularArrayRotation {
+
+    public static List<Integer> circularArrayRotation(List<Integer> a, int k, List<Integer> queries) {
+        List<Integer> result = new ArrayList<>();
+        int length = a.size();
+        k %= length;
+
+        for (int i = 0; i < queries.size(); i++) {
+            int index = (queries.get(i) - k + length) % length;
+            result.add(a.get(index));
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // Test 1
+        List<Integer> a = Arrays.asList(1, 2, 3);
+        int k = 2;
+        List<Integer> queries = Arrays.asList(0, 2);
+        List<Integer> result = circularArrayRotation(a, k, queries);
+        System.out.println(result.equals(Arrays.asList(2, 1))); 
+        
+        // Test 2
+        List<Integer> b = Arrays.asList(5, 6, 7, 8, 9);
+        int k2 = 3;
+        List<Integer> queries2 = Arrays.asList(1, 3);
+        List<Integer> result2 = circularArrayRotation(b, k2, queries2);
+        System.out.println(result2.equals(Arrays.asList(8, 5)));
+    }
+
+}
+
